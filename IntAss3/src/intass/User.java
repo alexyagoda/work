0988bcfd8d79c06644e794e86eca8f550c892153
alexyagoda <58227378+alexyagoda@users.noreply.github.com>
2019-11-26 11:52:06 -0500

    Add files via upload
    Uploaded 11/26 at 11:50 before any updates, there may be an updated version,
    Things to look at are Table2.java, Table2.form, User.java

---
+++ IntAss3/src/intass/User.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intass;
+
+/**
+ *
+ * @author 3399a
+ */
+public class User {
+        public String number;
+        public String fname;
+        public String lname;
+        public String teacher;       
+        public double balance;
+        
+        public User(String number, String fname, String lname, String teacher, double balance) {
+            this.number = number;
+            this.fname = fname;
+            this.lname = lname;
+            this.teacher = teacher;
+            this.balance = balance;
+                   
+        }
+}
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 3399a
+ */
+
+    
